208adff16f39cb70d63a5dbe3568dff09d7ce7f8
a1000rhan <1000rhan@gmail.com>
2022-03-07 23:08:22 +0300

    random cards

--- src/components/AccountItem.js
+++ src/components/AccountItem.js
@@ -2,12 +2,21 @@ import React from "react";
 import account from "../account.css";
 import { observer } from "mobx-react";
 import accountStore from "../Store/accountStore";
+import card1 from "../images/card-empty.png";
+import card2 from "../images/card-empty-red.png";
+import card3 from "../images/card-empty-black.png";
 function AccountItem({ account }) {
   if (accountStore.loading) return <h1>loading</h1>;
   console.log(account.accountNumber);
+
+  const cards = [card1, card2, card3];
+
+  const shuffledArray = cards.sort((a, b) => 0.5 - Math.random());
+
   return (
     <div>
-      <div className="backgroundCard">
+      <div className="card-box">
+        <img className="backgroundCard" src={shuffledArray[0]} alt="cards" />
         <p className="cardNumber">
           8803&nbsp;&nbsp; &nbsp;****&nbsp;&nbsp;&nbsp; ****&nbsp;&nbsp;&nbsp;
           {account.accountNumber
